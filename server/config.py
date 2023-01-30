6057afc0868b6b38afe273c50a8830aa5cea25aa
ARCTURUS <1638330246@qq.com>
2023-01-30 12:10:58 +0800

    feat: perfect docker deployment

--- server/config.py
+++ server/config.py
@@ -32,6 +32,8 @@ MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
 MYSQL_DB = os.getenv("MYSQL_DB", "fyjlsq")
 
+print(MYSQL_HOST, MYSQL_PORT)
+
 # SQLAlchemy
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"  # noqa E501
 SQLALCHEMY_ECHO = False  # do not show original SQL statement
